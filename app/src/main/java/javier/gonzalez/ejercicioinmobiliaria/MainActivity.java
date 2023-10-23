f76be1e27b7e65becc0be8676ee5dc8726cbd627
Javier2992 <123154776+Javier2992@users.noreply.github.com>
2023-10-23 10:33:37 +0200

    Initial commit

---
+++ app/src/main/java/javier/gonzalez/ejercicioinmobiliaria/MainActivity.java
@@ -0,0 +1,42 @@
+package javier.gonzalez.ejercicioinmobiliaria;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+
+
+
+import javier.gonzalez.ejercicioinmobiliaria.databinding.ActivityMainBinding;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+
+}
