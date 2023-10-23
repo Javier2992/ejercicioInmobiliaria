89eeacd728890aa91597ece25336de0adc6227f8
Javier2992 <123154776+Javier2992@users.noreply.github.com>
2023-10-23 11:25:19 +0200

    l

--- app/src/main/java/javier/gonzalez/ejercicioinmobiliaria/MainActivity.java
+++ app/src/main/java/javier/gonzalez/ejercicioinmobiliaria/MainActivity.java
@@ -1,8 +1,14 @@
 package javier.gonzalez.ejercicioinmobiliaria;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -18,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private ActivityMainBinding binding;
-
+    private ActivityResultLauncher<Intent> launcherAddInmueble;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        incializarLaunchers();
 
 
 
@@ -33,9 +40,22 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                launcherAddInmueble.launch(new Intent(MainActivity.this,AddInmuebleActivity.class));
             }
         });
+    }
+
+    private void incializarLaunchers() {
+        launcherAddInmueble = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult o) {
+                        //Resultado de volver a la actividad inmueble
+
+                    }
+                }
+        );
     }
 
 
